90cd0d15227a6be020dfad0e4bef3e39a5f51a92
Mauricio Rodas <mauriciorodascontacto@gmail.com>
2025-11-17 22:50:37 -0300

    Listar EstadoTurno

--- Software Veterinario/Negocio/NEstadoTurno.cs
+++ Software Veterinario/Negocio/NEstadoTurno.cs
@@ -9,5 +9,29 @@ namespace Negocio
 {
     public class NEstadoTurno
     {
+        public List<EstadoTurno> ListarEstadoTurno()
+        {
+            using (AccesoDatos datos = new AccesoDatos())
+            {
+                List<EstadoTurno> listaEstadoTurno = new List<EstadoTurno>();
+                try
+                {
+                    datos.Consulta("Select E.Id, E.Descripcion From ESTADOS_TURNO E");
+                    datos.EjecutarConsulta();
+                    while (datos.Lector.Read())
+                    {
+                        EstadoTurno e = new EstadoTurno();
+                        e.Id = (int)datos.Lector["Id"];
+                        e.Descripcion = (string)datos.Lector["Descripcion"];
+                        listaEstadoTurno.Add(e);
+                    }
+                    return listaEstadoTurno;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
